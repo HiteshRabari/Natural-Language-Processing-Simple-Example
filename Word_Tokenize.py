0f1242d1e2fb81d64521cb4eaea524689d001502
Hitesh Desai <hiteshcrabari@gmail.com>
2025-08-29 23:27:30 +0545

    Word_Tokenize.py

--- Word_Tokenize.py
+++ Word_Tokenize.py
@@ -1,1 +1,10 @@
+# TOKENIZATION
+import nltk
 
+nltk.download('punkt')
+
+from nltk.tokenize import word_tokenize
+
+text = "Hello, this is simple Word tokenize Program"
+tokens = word_tokenize(text)
+print(tokens)
